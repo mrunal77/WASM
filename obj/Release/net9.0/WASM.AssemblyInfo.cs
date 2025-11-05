06c8ec959df02b37bb1063566b1679b4000ce759
mrunal77 <mtmeshram77@outlook.com>
2025-11-05 16:34:22 +0530

    Yaml file update

--- obj/Release/net9.0/WASM.AssemblyInfo.cs
+++ obj/Release/net9.0/WASM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WASM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9565f08123e118906fb74ab9bfb4bce355995f4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1fa53bf223cc3b89b14063ee412358a28c97de2")]
 [assembly: System.Reflection.AssemblyProductAttribute("WASM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WASM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
